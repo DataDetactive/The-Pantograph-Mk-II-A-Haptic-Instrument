6670fdfcaa8953c0c8541439c19699d9a6688c14
DataDetactive <kareemelgohary98@gmail.com>
2024-03-12 08:27:50 +0100

    tutorial2

--- Tutorial_2/Test_tuto2.m
+++ Tutorial_2/Test_tuto2.m
@@ -1,5 +1,27 @@
 
-a1 = 63; a2 = 75; a3 = 75; a4 = 63; a5 = 25; t1 = 0.5; t5 = 0.6;epsilon=0.001;
+%4- J=df(theta)/dtheta
+% We could induce a little change in the angle delta(theta)
+% And compare J to delta(p)/delta(theta)= (f(theta+ delta(theta))- f(theta))/delta(theta)
+
+
+%6 tau= transpose(J)*f
+% Explanation: 
+% Virtual work:
+% A system in equilibrum have zero virtual work
+% It is expressed as delta_w=sum((tau_i)transpose*delta_q)=0, where delta_q corresponds to the joint displacement
+% Formula
+% To represent forces at the robot's end-effector, a Cartesian force is used, f.
+% The relationship tau= transpose(J)*f is established
+% Why virtual environment:
+% Working with working has limitations so we consider this environment
+% The formula comes from the necessity to ensure zero virtual work in a multibody system, linking Cartesian forces to joint torques.
+
+addpath('./Tutorial_1 /');
+% a1 = 63; a2 = 75; a3 = 75; a4 = 63; a5 = 25; t1 = 0.5; t5 = 0.6;epsilon=rand(1)*0.01
+a1 = 0.25; a2 = 0.25; a3 = 0.25; a4 = 0.25; a5 = 0.1; t1 = 0.7; t5 = 0.7;epsilon=rand(1)*0.01;
+vec_epsilon = rand(1,2)*0.01;
+
+
 % result= jacobian(a1, a2, a3, a4, a5, t1, t5);
 
 % Compute the forward kinematics result for the original joint angles
@@ -16,12 +38,19 @@ delta_p_expected = p_perturbed - p_original;
 
 % Compute the change in end-effector position predicted by the Jacobian matrix
 Jac = jacobian(a1, a2, a3, a4, a5, t1, t5);
-delta_theta = [epsilon; epsilon]; % Perturbation vector
-delta_p_actual = Jac * delta_theta;
+% delta_theta = [epsilon; epsilon];% Perturbation vector
+delta_theta = [epsilon,epsilon];
+delta_p_actual = Jac * delta_theta';
 
 % Compare the changes in end-effector position
-difference = norm(delta_p_expected - delta_p_actual);
+% difference = norm(delta_p_expected - delta_p_actual);
+disp("FKM(theta+delta theta)-FKM(theta)")
+disp(delta_p_expected)
+
+ disp("J*delta theta= " )
+disp(delta_p_actual)
 
 % Display the difference
-disp(['Difference between expected and actual change in end-effector position: ' num2str(difference)]);
+% disp(['Difference between expected and actual change in end-effector position: ' num2str(difference)]);
+
 
--- Tutorial_2/jacobian.m
+++ Tutorial_2/jacobian.m
@@ -1,19 +1,55 @@
+%1- Jacobian matrix of the robot
 
+% (Using d as partial derivate)
+% dp/dt= J*dtheta/dt
+% It gives: J= 	df/dtheta
+% Finaly, J= [[dx3/dtheta1 dx3/dtheta5][dy3/dtheta1 dy3/dtheta5]]
+
+%2- Differentiation of the FKM
+
+% Correction of the equations:
+%  dx2/dtheta1= -a1*sin(theta1)
+%  dx4/dtheta5= -a4*sin(thata5)
 
 function J = jacobian(a1, a2, a3, a4, a5, t1, t5)
     addpath('./Tutorial_1 /');
+
+
+    x1 = 0;
+    y1 = 0;
+
+
+    x2 = a1*cos(t1);
+    y2 = a1*sin(t1);
+
+    x4 = a4*cos(t5)-a5;
+    y4 = a4*sin(t5);
+
+    x5 = -a5;
+    y5 = 0;
+
+    P2 = [x2;y2];
+    P4 = [x4;y4];
+
+    P2Ph = (a2^2-a3^2 + norm(P4-P2)^2) / (2 * norm(P4-P2));
+    Ph   = P2 + (P2Ph/norm(P2-P4)) * (P4 - P2);
+    P3Ph = sqrt(a2^2 - (P2Ph)^2);
+
+    x3 = Ph(1) + (P3Ph/norm(P2-P4)) * (y4 - y2);
+    y3 = Ph(2) - (P3Ph/norm(P2-P4)) * (x4 - x2);
+
+    P3 = [x3;y3];
     
-    p = forward_kinematics(a1, a2, a3, a4, a5, t1, t5);
-    x3 = p(1);
-    y3 = p(2);
+   
+  
 
-    d = norm([a1*cos(t1); a1*sin(t1)] - [a4*cos(t5)-a5; a4*sin(t5)]);
-    b = norm([a1*cos(t1); a1*sin(t1)] - [x3; y3]);
-    h = norm([x3; y3] - [a4*cos(t5)-a5; a4*sin(t5)]);
+    d = norm(P2-P4);
+    b = norm(P2-Ph);
+    h = norm(P3-Ph);
 
-    del1_x2 = -a1*sin(t1);  % %NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
+    del1_x2 = -a1*sin(t1);   %NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
     del1_y2 = a1*cos(t1);
-    del5_x4 = -a4*sin(t5);  % %NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
+    del5_x4 = -a4*sin(t5);   %NOTE: THE AUTHOR FORGOT NEGATIVE SIGN IN THE PAPER
     del5_y4 = a4*cos(t5);
 
     del1_y4 = 0;
